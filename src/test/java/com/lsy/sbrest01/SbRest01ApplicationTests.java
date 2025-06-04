2e76c6c659d896976f915c69ec7fbc0675f76d73
이상엽 <infightersy@naver.com>
2025-06-04 19:36:08 +0900

    first commit

---
+++ src/test/java/com/lsy/sbrest01/SbRest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsy.sbrest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
