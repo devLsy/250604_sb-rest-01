8754c74362198f1b49918176e1455bf27fabb1c8
이상엽 <infightersy@naver.com>
2025-06-10 07:41:39 +0900

    swagger 추가

--- src/main/java/com/lsy/sbrest01/member/model/MemberVo.java
+++ src/main/java/com/lsy/sbrest01/member/model/MemberVo.java
@@ -1,6 +1,5 @@
 package com.lsy.sbrest01.member.model;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
